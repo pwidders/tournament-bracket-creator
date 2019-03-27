8af5f74397b26ece8a4ae4673c862abb0d2812e8
Pete Widders <pwidders@hotmail.com>
2019-03-26 21:17:26 -0700

    Basic UI elements and beginning JS

---
+++ app.js
@@ -0,0 +1,7 @@
+class UI {
+  addTeamToList(team) {
+    const list = document.getElementById('teamsList');
+    // create a new li element
+    const listItem = document.createElement('li')
+  }
+}
