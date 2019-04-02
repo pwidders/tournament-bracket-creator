faebd59acb1c16d3301eea36fc20b65776ea6c0e
Pete Widders <pwidders@hotmail.com>
2019-04-01 22:26:05 -0700

    basic ui foundation

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-class UI {
-  addTeamToList(team) {
-    const list = document.getElementById('teamsList');
-    // create a new li element
-    const listItem = document.createElement('li')
+class Player {
+  constructor(title) {
+    this.title = title;
   }
 }
+
+// Global variables
+const teams = [];
+
+class UI {
+  // Function add team
+  addTeamToList(player) {
+
+  }
+}
+
+// Event listener for add team to list
+document.getElementById('addTeam').addEventListener('submit', function(e) {
+  const teamName = document.getElementById('teamName').value;
+
+  // Instantiate Player
+  const player = new Player(teamName);
+  // console.log(player);
+  teams.push(player);
+  console.log(teams);
+
+  // Instantiate UI
+
+  
+  e.preventDefault();
+})
